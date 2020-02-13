96c83ffcab43c7d76c527babdc810df13c8ae58e
Jamila Aliyeva and Wei Dai <wei@>
2020-02-12 16:49:08 -0800

    background img.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import Header from './Header';
 // import TicketList from './TicketList';
-import MarketScheduleList from './MarketScheduleList';
+// import MarketScheduleList from './MarketScheduleList';
 // import SeasonalProduceList from './SeasonalProduceList';
 import { Switch, Route } from 'react-router-dom';
 import NewMarketScheduleForm from './NewMarketScheduleForm';
+// import NavBar from './NavBar';
+import Background from './Background';
 
 function App(){
   return (
     <div>
       <Header/>
+      {/* <NavBar/> */}
       <Switch>
-        <Route exact path='/' component={MarketScheduleList} />
+        {/* <Route exact path='/' component={MarketScheduleList} /> */}
         <Route path='/newmarketscheduleform' component={NewMarketScheduleForm} />
       </Switch>
+      <Background/>
     </div>
   );
 }
